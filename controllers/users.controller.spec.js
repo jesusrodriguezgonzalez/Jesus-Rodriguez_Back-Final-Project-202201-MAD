9d58fdafd2288580b0f284a096694cf76ff09aac
Jesus Rodriguez <jesusrodriguezgonzalezgr@gmail.com>
2022-03-14 16:56:30 +0100

    Added registerUser controllers and test

--- controllers/users.controller.spec.js
+++ controllers/users.controller.spec.js
@@ -1,11 +1,13 @@
-import { getAllUsers } from '../controllers/users.controllers.js';
+import { getAllUsers, registerUser } from '../controllers/users.controllers.js';
 import { User } from '../models/user.models.js';
 import { mockRequest, mockResponse } from '../utils/interceptos';
+import bcrypt from 'bcryptjs';
 
 jest.mock('../models/user.models.js');
+jest.mock('bcryptjs');
 
 describe("Given controllers' ", () => {
-    describe('When getAllUsers receives data without errors', () => {
+    describe('Testing  getAllUsers ', () => {
         test('should return correct mockResolvedValue', async () => {
             User.find.mockResolvedValue([
                 {
@@ -29,17 +31,42 @@ describe("Given controllers' ", () => {
                 },
             ]);
         });
-    });
-    describe('When getAllUsers throws an error', () => {
-        beforeEach(() => {
-            User.find.mockRejectedValue('Test error');
-        });
         test('should return correct mockRejectedValue', async () => {
+            User.find.mockRejectedValue('Test error');
             try {
                 await getAllUsers();
             } catch (e) {
                 expect(e).toEqual(new Error('Test error'));
             }
+        });
+    });
+
+    describe('Testing registerUser', () => {
+        let req = mockRequest();
+        const res = mockResponse();
+        let next = jest.fn();
+        describe('And it works (promise is resolved)', () => {
+            beforeEach(() => {
+                req.body = { name: 'Jesus', passwd: '1234' };
+                bcrypt.hashSync.mockResolvedValue('encrypted1234');
+                User.mockReturnValue({
+                    name: 'Jesus',
+                    passwd: 'encrypted1234',
+                    id: 1,
+                });
+            });
+            test('Then call json', async () => {
+                const userMock = {
+                    name: 'Jesus',
+                    passwd: '12345',
+                };
+                User.create.mockResolvedValue(userMock);
+                await registerUser(req, res, next);
+                await expect(res.json).toHaveBeenCalledWith({
+                    name: 'Jesus',
+                    passwd: '12345',
+                });
+            });
         });
     });
 });
--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -1,6 +1,8 @@
 import { User } from '../models/user.models.js';
 import { createError } from '../services/create-error.js';
 import { mongoConnect } from '../services/connection';
+import { createToken, verifyToken } from '../services/auth.js';
+import bcrypt from 'bcryptjs';
 export const getAllUsers = async (req, res, next) => {
     await mongoConnect();
     try {
@@ -8,5 +10,21 @@ export const getAllUsers = async (req, res, next) => {
         res.json(resp);
     } catch (err) {
         throw new Error(err);
+    }
+};
+
+export const registerUser = async (req, resp, next) => {
+    if (req.body) {
+        try {
+            const encryptedPasswd = bcrypt.hashSync(req.body.passwd);
+            const userData = { ...req.body, passwd: encryptedPasswd };
+            const result = await User.create(userData);
+            resp.json(result);
+        } catch (error) {
+            next(createError(error));
+        }
+    } else {
+        resp.status(400);
+        resp.json({ message: 'Error creating user' });
     }
 };
